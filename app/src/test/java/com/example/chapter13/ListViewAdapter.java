6aed02c51f792006a77bdad4641a476acde5da15
DonStreetKing <donstreetking@gmail.com>
2021-12-08 19:26:24 +0700

    Tugas chapter 13

---
+++ app/src/test/java/com/example/chapter13/ListViewAdapter.java
@@ -0,0 +1,38 @@
+package com.example.chapter13;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class ListViewAdapter extends ArrayAdapter<listitem> {
+    private List<listitem> productItemList;
+    private Context context;
+
+    public ListViewAdapter(List<listitem> productItemList, Context context) {
+        super(context, R.layout.list_item, productItemList);
+        this.productItemList = productItemList;
+        this.context = context;
+    }
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View listViewItem = inflater.inflate(R.layout.list_item, null, true);
+
+        TextView textVIewID = listViewItem.findViewById(R.id.textviewID);
+        TextView textViewNamaItem = listViewItem.findViewById(R.id.textviewNamaItem);
+        TextView textViewHarga = listViewItem.findViewById(R.id.textviewHarga);
+
+        listitem listItem = productItemList.get(position);
+
+        textVIewID.setText(listItem.getID());
+        textViewNamaItem.setText(listItem.getNamaItem());
+        textViewHarga.setText(listItem.getNamaItem());
+        return listViewItem;
+    }
+}
